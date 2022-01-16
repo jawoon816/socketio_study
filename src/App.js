76c8baa9b5c8efd9725bfac4ea750d6b615ac608
jawoon816 <jawoon816@gmail.com>
2022-01-16 20:15:16 +0900

    socketio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./Chat";
+
+const socket = io.connect("http://localhost:3001"); //서버랑연결
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!showChat ? ( //채팅 중일때만 채팅방 보이게 하기
+        <div className="joinChatContainer">
+          <h3>Join A Chat</h3>
+          <input
+            type="text"
+            placeholder="John..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          ></input>
+          <input
+            type="text"
+            placeholder="RooM Id..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          ></input>
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room}></Chat>
+      )}
     </div>
   );
 }
---
+++ src/Chat.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+
+const Chat = ({ socket, username, room }) => {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      //메세지를 주고 받을 때는
+      //방정보, 유저, 메세지내용, 시간 필요
+      //socket.on(send_message)부분이랑 데이터 통신
+      const messageData = {
+        room: room,
+        author: username,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+
+      await socket.emit("send_message", messageData);
+    }
+  };
+
+  useEffect(() => {
+    //소켓 서버가 바뀔 때 마다 실행
+    socket.on("receive_message", (data) => {
+      setMessageList((list) => [...list, data]);
+    });
+  }, [socket]);
+
+  return (
+    <div className="chat-window">
+      <div className="chat-header">
+        <p>Live Chat</p>
+      </div>
+      <div className="chat-body">
+        {messageList.map((messageContent) => {
+          return <h1>{messageContent.message}</h1>;
+        })}
+      </div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          placeholder="Hey..."
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+        />
+        {/* 버튼 눌렀을 때 함수실행해서 메세지 보내기 */}
+        <button onClick={sendMessage}>&#9658;</button>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
